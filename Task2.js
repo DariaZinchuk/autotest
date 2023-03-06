ccb36ec098330401de6965e161aa230f460d8a5a
Daria Zinchuk <dashazinchuk@ukr.net>
2023-03-06 21:44:37 +0200

    second practical task

---
+++ Task2.js
@@ -0,0 +1,19 @@
+const str = "hello";
+const bool = true;
+const num = 24;
+
+const addResult = str + bool;
+const addResult1 = str + num;
+const addResult2 = num + bool;
+
+const multResult = str * bool;
+const multResult1 = str * num;
+const multResult2 = num * bool;
+
+const divResult = str / bool;
+const divResult1 = str / num;
+const divResult2 = num / bool;
+
+const convResult = Boolean (str);
+const convResult1 = Number (bool);
+const convResult2 = String (num);
